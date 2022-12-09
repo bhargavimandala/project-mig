3436eeafa4bf7281d224c2279952bdde5ae61d4a
Bhargavi <bhargavimandala@gmail.com>
2022-12-09 09:36:11 +0000

    Added ec2-elb in dev environment

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "dev_vpc" {
-    source = "git@github.com:bhargavimandala/terraform-aws-vpc-module.git"
+    source = "git@github.com:bhargavimandala/terraform-aws-vpc-module.git?ref=v1.3"
     cidr_block = var.dev_cidr_block
     product = var.product
     environment = var.environment
@@ -29,5 +29,24 @@ module "dev_ec2" {
     subnet_ids = var.subnet_ids
     terraform_repo = var.terraformrepo
     costcode = var.costcode
+}
 
+#### ec2-elb dev module #######
+
+module "dev_ec2-elb" {
+    source = "git@github.com:bhargavimandala/terraform-aws-ec2-elb-module.git"
+    ami = var.ami
+    instance_type = var.instance_type
+    product = var.product
+    environment = var.environment
+    key_name = var.key_name
+    vpc_id = var.dev_vpc_id
+    ec2_ebs_volume_size = var.dev_volume_size
+    ec2_device_names = var.dev_device_names
+    ec2_count = var.ec2_count
+    ebs_volume_count = var.ebs_volume_count
+    availability_zones = var.az
+    subnet_ids = var.subnet_ids
+    terraform_repo = var.terraformrepo
+    costcode = var.costcode
 }
